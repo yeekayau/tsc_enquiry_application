464467300d77376a2162bbee687b5d8527b79496
unknown <yeyau@DALDE93.shepherdcentre.local>
2015-10-02 16:26:12 +1000

    success in adding value to db

--- dbconnect.py
+++ dbconnect.py
@@ -2,14 +2,18 @@ import pypyodbc
 
 cnxn = pypyodbc.connect('Driver={SQL Server Native Client 10.0};'
 						'server=DASDB01;'
-						'database=IA_ThankQ;'
+						'database=IA_CDIS;'
 						'trusted_connection=yes;')
 
 cursor = cnxn.cursor()
 
-cursor.execute("select top 10 SOURCECODE from Selections")
+cursor.execute("select top 10 ApplicationUser from WindowsUsers")
 
 row = cursor.fetchall()
 if row:
 	print(row)
+
+cursor.execute("insert into Form_test(p_guid, Lastname, Firstname) values (Newid(), 'Yau', 'Yeeka')")
+cnxn.commit()
+
 
