6dba37806c81a670bfeca70e411b5b5e5d79598c
unknown <yeyau@DALDE93.shepherdcentre.local>
2015-10-02 13:52:02 +1000

    can access values in XML

--- fsapi.py
+++ fsapi.py
@@ -11,13 +11,41 @@ fs_familysurvey_apibase_url = 'https://fs30.formsite.com/api/users/TheShepherdCe
 x = urllib.request.urlopen(fs_familysurvey_apibase_url + 'results?fs_api_key='
 							+ fs_familysurvey_api_key)
 
+# parses the URL data as an XML element tree
 tree = ElementTree.parse(x)
 
 # The root element would be the top tag - in our case, this would be the fs_response tag
-root = tree.getroot()
+fs_response = tree.getroot()
 
-for child in root:
+# This is the 2nd level tag, so it's only <results> and <total_results> tags which are
+# on the same level
+for child in fs_response:
 	print (child.tag, child.attrib)
 
+#iter_ = tree.getiterator()
+#for elem in iter_:
+#    print (elem.tag)
 
+top_level = fs_response.getchildren()
+
+for next_level in top_level:
+    results = next_level.getchildren()
+    
+    # No result.text is printed as the element itself (which is each result) does not have
+    # an atribute
+    for result in results:
+        print (result.tag, result.attrib)
+        # next level of tags in each result, includes <metas> and <items tags>,
+        # No text is printed as the tag itself does not have a value
+
+        for metas_items in result:
+
+            for meta in metas_items:
+
+                print (meta.tag, meta.attrib, meta.text)
+
+                for row in meta:
+                    print(row.tag, row.attrib, row.text)
+                    for value in row:
+                        print(value.tag, value.attrib, value.text)
 # print(x.read())
