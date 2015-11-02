a9b0a0a7a0c95e95c90576ba8cc5106256a69768
unknown <yeyau@DALDE93.shepherdcentre.local>
2015-11-02 18:15:00 +1100

    fixed the sq string, but need to fix insert statement

--- updateEnquiries.py
+++ updateEnquiries.py
@@ -1,6 +1,5 @@
 
 
-# AppForm Test
 from formXMLDataHandler import getEnquiryFormData
 import pypyodbc
 import urllib.request
@@ -18,6 +17,7 @@ tree = ElementTree.parse(x)
 
 enquiry_list = getEnquiryFormData(tree)
 
+# Lets just see what we got
 for dictionary in enquiry_list:
     print('********************************************************************')
     for key, name in dictionary.items():
@@ -25,20 +25,134 @@ for dictionary in enquiry_list:
  
 # ----------------------------------------
 # Create connection to DB
-# cnxn = pypyodbc.connect('Driver={SQL Server Native Client 10.0};'
-# 						'server=DASDB01;'
-# 						'database=IA_CDIS;'
-# 						'trusted_connection=yes;')
+cnxn = pypyodbc.connect('Driver={SQL Server Native Client 10.0};'
+						'server=DASDB01;'
+						'database=IA_CDIS;'
+						'trusted_connection=yes;')
 
-# cursor = cnxn.cursor()
+cursor = cnxn.cursor()
+# 37 items
+sql = ('insert into Enquires('
+'result_status, '
+'date_start, '
+'date_finish, '
+'user_browser, '
+'user_os, '
+'user_referrer, '
+'client_p_guid, '
+'Lastname, '
+'Firstname, '
+'lastname, '
+'dateofbirth, '
+'caregiver_firstname, ' 
+'caregiver_lastname, '
+'relationship_to_child, '
+'suburb, '
+'caregiver_phone, '
+'caregiver_alt_phone, '
+'caregiver_email, '
+'preferred_contact_method, '
+'[p_c_day_mon_morning], '
+'[p_c_day_mon_lunch], '
+'[p_c_day_mon_afternoon], '
+'[p_c_day_tues_morning], '
+'[p_c_day_tues_lunch], '
+'[p_c_day_tues_afternoon], '
+'[p_c_day_wed_morning], '
+'[p_c_day_wed_lunch], '
+'[p_c_day_wed_afternoon], '
+'[p_c_day_thurs_morning], '
+'[p_c_day_thurs_lunch], '
+'[p_c_day_thurs_afternoon], '
+'[p_c_day_fri_morning], '
+'[p_c_day_fri_lunch], '
+'[p_c_day_fri_afternoon], '
+'[how_can_we_help], '
+'joined_program,'
+'not_joined_reason'
+'values '
+'({},{},{},{},{},{},Newid(),{},{},{},{},{},{},{},{},{},{},{},{},'
+'{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},0,NULL)')
 
-# sql = 'insert into Form_test(p_guid, Lastname, Firstname) values (Newid(),{},{})'
+for result in enquiry_list:
+    
+    if result.get('result_status'):
+        result_status = "'{}'".format(result.get('result_status')) 
+    else:
+        result_status = 'Incomplete'
 
-# for result in person_list:
-#     Lastname = "'{}'".format(result.get('Lastname'))
-#     Firstname = "'{}'".format(result.get('Firstname'))
-#     insert = sql.format(Lastname, Firstname)
-#     print(insert)
+    date_start = "'{}'".format(result.get('date_start'))
+    date_finish = "'{}'".format(result.get('date_finish')) 
+    user_browser = "'{}'".format(result.get('user_browser'))
+    user_os = "'{}'".format(result.get('user_os')) 
+    user_referrer = "'{}'".format(result.get('user_referrer'))
+    Lastname = "'{}'".format(result.get('Lastname'))
+    Firstname = "'{}'".format(result.get('Firstname'))
+    dateofbirth = "'{}'".format(result.get('dateofbirth'))
+    caregiver_firstname = "'{}'".format(result.get('caregiver_firstname'))
+    caregiver_lastname = "'{}'".format(result.get('caregiver_lastname'))
+    relationship_to_child = "'{}'".format(result.get('relationship_to_child'))
+    suburb = "'{}'".format(result.get('suburb'))
+    caregiver_phone = "'{}'".format(result.get('caregiver_phone'))
+    caregiver_alt_phone = "'{}'".format(result.get('caregiver_alt_phone'))
+    caregiver_email = "'{}'".format(result.get('caregiver_email'))
+    preferred_contact_method = "'{}'".format(result.get('preferred_contact_method'))
+
+    if result.get('p_c_day_mon_morning'):
+        p_c_day_mon_morning = "'{}'".format(result.get('p_c_day_mon_morning'))
+    else:
+        p_c_day_mon_morning = 0
+        
+    p_c_day_mon_lunch = "'{}'".format(result.get('p_c_day_mon_lunch'))
+    p_c_day_mon_afternoon = "'{}'".format(result.get('p_c_day_mon_afternoon'))
+    p_c_day_tues_morning = "'{}'".format(result.get('p_c_day_tues_morning'))
+    p_c_day_tues_lunch = "'{}'".format(result.get('p_c_day_tues_lunch'))
+    p_c_day_tues_afternoon = "'{}'".format(result.get('p_c_day_tues_afternoon'))
+    p_c_day_wed_morning = "'{}'".format(result.get('p_c_day_wed_morning'))
+    p_c_day_wed_lunch = "'{}'".format(result.get('p_c_day_wed_lunch'))
+    p_c_day_wed_afternoon = "'{}'".format(result.get('p_c_day_wed_afternoon'))
+    p_c_day_thurs_morning = "'{}'".format(result.get('p_c_day_thurs_morning'))
+    p_c_day_thurs_lunch = "'{}'".format(result.get('p_c_day_thurs_lunch'))
+    p_c_day_thurs_afternoon = "'{}'".format(result.get('p_c_day_thurs_afternoon'))
+    p_c_day_fri_morning = "'{}'".format(result.get('p_c_day_fri_morning'))
+    p_c_day_fri_lunch = "'{}'".format(result.get('p_c_day_fri_lunch'))
+    p_c_day_fri_afternoon = "'{}'".format(result.get('p_c_day_fri_afternoon'))
+    how_can_we_help = "'{}'".format(result.get('how_can_we_help'))
+
+    insert = sql.format(result_status, 
+                        date_start, 
+                        date_finish,
+                        user_browser, 
+                        user_os, 
+                        user_referrer, 
+                        Firstname, 
+                        Lastname, 
+                        dateofbirth, 
+                        caregiver_firstname, 
+                        caregiver_lastname, 
+                        relationship_to_child, 
+                        suburb, 
+                        caregiver_phone, 
+                        caregiver_alt_phone, 
+                        caregiver_email, 
+                        preferred_contact_method,
+                        p_c_day_mon_morning,
+                        p_c_day_mon_lunch,
+                        p_c_day_mon_afternoon,
+                        p_c_day_tues_morning,
+                        p_c_day_tues_lunch,
+                        p_c_day_tues_afternoon,
+                        p_c_day_wed_morning,
+                        p_c_day_wed_lunch,
+                        p_c_day_wed_afternoon,
+                        p_c_day_thurs_morning,
+                        p_c_day_thurs_lunch,
+                        p_c_day_thurs_afternoon, 
+                        p_c_day_fri_morning, 
+                        p_c_day_fri_lunch, 
+                        p_c_day_fri_afternoon, 
+                        how_can_we_help)
+    print(insert)
 #     cursor.execute(insert)
 #     cnxn.commit()
 
