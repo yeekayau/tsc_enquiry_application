7183d4e4624c640da681ab7270cab297e1619d1c
unknown <yeyau@DALDE93.shepherdcentre.local>
2015-12-01 17:41:15 +1100

    some basic functionality createdgit add .

--- enquiry_app.py
+++ enquiry_app.py
@@ -12,18 +12,25 @@ app = Flask(__name__)
 cnxn = pypyodbc.connect('Driver={SQL Server Native Client 10.0};'
 						'server=DASDB01;'
 						'database=IA_CDIS;'
-						'trusted_connection=yes;')
+						'trusted_connection=yes;'
+						'MARS_Connection=yes;')
 
 cursor = cnxn.cursor()
-
 cursor.execute('select * from Enquiries')
-
-rows = cursor.fetchall()
 
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	enquiry_list = []
+    
+	cursor = cnxn.cursor()
+	cursor.execute('select * from Enquiries')
+	
+	for row in cursor.fetchall():
+		enquiry_dict = dict(id = row[0], date = row[3], firstname = row[9])
+		enquiry_list.append(enquiry_dict)
+	
+	return render_template('index.html', rows = enquiry_list)
 	
 
 
