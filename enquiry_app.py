3217214f9d0626d7758d358d13680798dd9e0ac3
unknown <yeyau@DALDE93.shepherdcentre.local>
2015-12-03 15:09:55 +1100

    two pages to the app

--- enquiry_app.py
+++ enquiry_app.py
@@ -15,23 +15,45 @@ cnxn = pypyodbc.connect('Driver={SQL Server Native Client 10.0};'
 						'trusted_connection=yes;'
 						'MARS_Connection=yes;')
 
-cursor = cnxn.cursor()
-cursor.execute('select * from Enquiries')
-
 
 @app.route('/')
 def index():
+
 	enquiry_list = []
     
 	cursor = cnxn.cursor()
 	cursor.execute('select * from Enquiries')
 	
+	# cursor fetchall() retrieves a list of tuples
 	for row in cursor.fetchall():
-		enquiry_dict = dict(id = row[0], date = row[3], firstname = row[9])
+		enquiry_dict = dict(enq_id = row[0], enq_date = row[3], firstname = row[9],
+							lastname = row[10],
+							dateofbirth = row[11],
+							relationship_to_child = row[14],
+							caregiver_firstname = row[12],
+							caregiver_lastname = row[13]
+							)
+		enquiry_list.append(enquiry_dict)
+
+	return render_template('index.html', rows=enquiry_list)
+	
+@app.route('/enquiry/<int:enq_id>')
+def view_enq(enq_id):
+	enquiry_list = []
+	cursor = cnxn.cursor()
+	cursor.execute("""select * from Enquiries where enquiry_id = ?""", [str(enq_id)] )
+
+	for row in cursor.fetchall():
+		enquiry_dict = dict(enq_id = row[0], enq_date = row[3], firstname = row[9],
+							lastname = row[10],
+							dateofbirth = row[11],
+							relationship_to_child = row[14],
+							caregiver_firstname = row[12],
+							caregiver_lastname = row[13]
+							)
 		enquiry_list.append(enquiry_dict)
 	
-	return render_template('index.html', rows = enquiry_list)
-	
+	return render_template('view_enquiry.html', enquiry=enquiry_list)	
 
 
 if __name__ == '__main__':
